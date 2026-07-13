061eaa4087cf8413feb25f51352416163789df5d
Mario Alcala <malcala45@gmail.com>
2026-07-13 18:41:46 -0500

    Fix ModMenu mapping using modApi and native AutoConfig call

--- src/main/java/autotorch/autotorch/client/ModMenuIntegration.java
+++ src/main/java/autotorch/autotorch/client/ModMenuIntegration.java
@@ -2,29 +2,12 @@ package autotorch.autotorch.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.screens.Screen;
-
-import java.lang.reflect.Method;
-import java.util.function.Supplier;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-            try {
-                Class<?> autoConfigClass = Class.forName("me.shedaniel.autoconfig.AutoConfig");
-                for (Method method : autoConfigClass.getMethods()) {
-                    if (method.getName().equals("getConfigScreen") && method.getParameterCount() == 2) {
-                        @SuppressWarnings("unchecked")
-                        Supplier<Screen> supplier = (Supplier<Screen>) method.invoke(null, ModConfig.class, parent);
-                        return supplier.get();
-                    }
-                }
-                return parent;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return parent;
-            }
-        };
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
     }
 }
