9622f5e9070e40e0b74df3ffe463df69b8001877
Mario Alcala <malcala45@gmail.com>
2026-07-13 19:22:08 -0500

    Fix ModMenu integration using AutoConfigClient

--- src/main/java/autotorch/autotorch/client/ModMenuIntegration.java
+++ src/main/java/autotorch/autotorch/client/ModMenuIntegration.java
@@ -2,12 +2,12 @@ package autotorch.autotorch.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.minecraft.client.gui.screens.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
